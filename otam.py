3a99103fddebcf4e675ea4e517722053997183b3
MMZaini <mahdizainipro@gmail.com>
2021-07-11 01:20:39 +0100

    fix grammar

--- otam.py
+++ otam.py
@@ -5,7 +5,7 @@ import time
 
 
 # set random number and print intro
-print("Let's play a game. I'm thinking of a number between 1 and 1 million. i will give you clues on how what the answer is (answer is not a decimal!)\n")
+print("Let's play a game. I'm thinking of a number between 1 and 1 million. i will give you clues on what the answer is or is not! (answer is not a decimal!)\n")
 ranum = random.randint(1,1000000)
 tries = 0
 
@@ -34,6 +34,7 @@ vs = input("Would you like to see your previous scores? yes or no\n")
 if vs == "yes":        
     f = open("scores.txt", "r")
     print(f.read())
+time.sleep(1)
 input("Press enter 3x to exit program")
 input("Press enter 2x to exit program")
 input("Press enter 1x to exit program")
